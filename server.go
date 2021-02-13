ef7910b8c8d7739ef9faa4e19b8d1b767b307fce
Shion Aoki <shio0321pc@gmail.com>
2021-02-13 14:00:21 +0000

    add

--- server.go
+++ server.go
@@ -2,18 +2,33 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"strings"
 )
 
 func main() {
-	http.HandleFunc("/", sayhelloName)       //アクセスのルーティングを設定します。
+	http.HandleFunc("/", sayhelloName) //アクセスのルーティングを設定します。
+	http.HandleFunc("/login", login)
 	err := http.ListenAndServe(":9090", nil) //監視するポートを設定します。
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
+
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method) //リクエストを取得するメソッド
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("login.gtpl")
+		t.Execute(w, nil)
+	} else {
+		//ログインデータがリクエストされ、ログインのロジック判断が実行されます。
+		fmt.Println("username:", r.Form["username"])
+		fmt.Println("password:", r.Form["password"])
+	}
+}
+
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()       //オプションを解析します。デフォルトでは解析しません。
 	fmt.Println(r.Form) //このデータはサーバのプリント情報に出力されます。
