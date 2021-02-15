e7923ee8b9f9b29bafb56224ceb7809fa14c2d9c
Shion Aoki <shio0321pc@gmail.com>
2021-02-15 14:21:59 +0000

    next tutorial

--- server.go
+++ server.go
@@ -1,43 +1,21 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
 	"log"
 	"net/http"
-	"strings"
 )
-
-func main() {
-	http.HandleFunc("/", sayhelloName) //アクセスのルーティングを設定します。
-	http.HandleFunc("/login", login)
-	err := http.ListenAndServe(":9090", nil) //監視するポートを設定します。
+ 
+func viewHandler(writer http.ResponseWriter, request *http.Request) {
+	message := []byte("Hello World!")
+	// Add message to the response
+	_, err := writer.Write(message)
 	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+		log.Fatal(err)
 	}
 }
-
-func login(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("method:", r.Method) //リクエストを取得するメソッド
-	if r.Method == "GET" {
-		t, _ := template.ParseFiles("html/login.gtpl")
-		t.Execute(w, nil)
-	} else {
-		//ログインデータがリクエストされ、ログインのロジック判断が実行されます。
-		fmt.Println("username:", r.Form["username"])
-		fmt.Println("password:", r.Form["password"])
-	}
-}
-
-func sayhelloName(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()       //オプションを解析します。デフォルトでは解析しません。
-	fmt.Println(r.Form) //このデータはサーバのプリント情報に出力されます。
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
-	}
-	fmt.Fprintf(w, "Hello astaxie!") //ここでwに入るものがクライアントに出力されます。
+ 
+func main() {
+	// register hander
+	http.HandleFunc("/hello", viewHandler)
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
