9de9ff3595dc6295a06af98647cc3c2f002a4b20
Shion Aoki <shio0321pc@gmail.com>
2021-02-17 01:27:22 +0000

    add post view

--- server.go
+++ server.go
@@ -1,29 +1,29 @@
 package main
- 
+
 import (
 	"log"
 	"net/http"
 )
- 
+
 func write(writer http.ResponseWriter, message string) {
 	_, err := writer.Write([]byte(message))
 	if err != nil {
 		log.Fatal(err)
 	}
 }
- 
+
 func one(writer http.ResponseWriter, request *http.Request) {
 	write(writer, "One")
 }
- 
+
 func two(writer http.ResponseWriter, request *http.Request) {
 	write(writer, "Two")
 }
- 
+
 func three(writer http.ResponseWriter, request *http.Request) {
 	write(writer, "Three")
 }
- 
+
 func main() {
 	http.HandleFunc("/one", one)
 	http.HandleFunc("/two", two)
